5954a3d4a76e14ef4adac2a17566ded69b2a0438
NewtonAdhikari <newtonadk@gmail.com>
2022-07-05 07:04:25 +0545

    helper functions and unit tests, step3 completed

--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -7,4 +7,11 @@ const totalLikes = (blogs) => {
     return blogs.reduce((first, {likes}) => first + likes, 0)
 }
 
-module.exports = {dummy, totalLikes};
+const favoriteBlog = (blogs) => {
+    if(!blogs.length) return null;
+    const maxLikes = blogs.reduce((max, {likes}) => likes > max ? likes : max, 0);
+    console.log(maxLikes);
+    return blogs.find(({likes}) => likes === maxLikes);
+}
+
+module.exports = {dummy, totalLikes, favoriteBlog};
