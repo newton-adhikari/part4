be6b634743d859c602c6aa89e624a73c55656623
NewtonAdhikari <newtonadk@gmail.com>
2022-07-05 20:41:53 +0545

    Blog list expansions, step2 completed

--- blog-list/controllers/blogRouter.js
+++ blog-list/controllers/blogRouter.js
@@ -19,9 +19,15 @@ router.post("/", async (req, res) => {
 router.delete("/:id", async(req, res) => {
     const blog = await Blog.findById(req.params.id);
     if(!blog) return res.status(400).json({error: "post not found"});
-    
+
     await Blog.findByIdAndRemove(req.params.id);
     res.send();
+})
+
+router.put("/:id", async(req, res) => {
+    const updated = await Blog.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+    if(!updated) res.status(400).json({error: "post not found"});
+    res.json(updated);
 })
 
 module.exports = router;
