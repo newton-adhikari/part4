8dadbd34ea96b508a03c6abed2d7b4efeb7dd610
NewtonAdhikari <newtonadk@gmail.com>
2022-07-05 06:41:53 +0545

    helper functions and unit tests, step2 completed

--- blog-list/utils/list_helper.js
+++ blog-list/utils/list_helper.js
@@ -2,4 +2,9 @@ const dummy = (blogs) => {
     return 1;
 }
 
-module.exports = {dummy};
+const totalLikes = (blogs) => {
+    if(!blogs.length) return 0;
+    return blogs.reduce((first, {likes}) => first + likes, 0)
+}
+
+module.exports = {dummy, totalLikes};
