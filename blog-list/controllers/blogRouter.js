e01a28ebf74bdbc785507ce72bb21a4bc7afce5a
NewtonAdhikari <newtonadk@gmail.com>
2022-07-07 09:36:33 +0545

    bloglist expansion, step7 completed

--- blog-list/controllers/blogRouter.js
+++ blog-list/controllers/blogRouter.js
@@ -1,28 +1,46 @@
 const router = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+const tokenExtractor = (req) => {
+    const token = req.get("authorization");
+    if(token && token.toLowerCase().startsWith("bearer")) {
+        return token.substring(7);
+    }
+    return null;
+}
 
 router.get("/", async (req, res) => {
     const blogs = await Blog.find({}).populate("user", {username: 1, name: 1});
     res.json(blogs);
 })
 
-router.post("/", async (req, res) => {
-    const {title, author, url, userId} = req.body;
+router.post("/", async (req, res, next) => {
+    try {
+        const token = tokenExtractor(req);
+        if(!token) return res.status(401).json({error: "unverified user"});
     
-    const user = await User.findById(userId);
-    if(!user) return res.status(400).json({error: "not authorized"});
-
-    if(!title || !url) return res.status(400).json({error: "missing title or url"});
+        const decoded = jwt.verify(token, process.env.SECRET);
+        const {title, author, url} = req.body;
+        
+        const user = await User.findById(decoded.id);
+        if(!user) return res.status(400).json({error: "not authorized"});
     
-    const blog = new Blog(req.body);
-    blog.user = user._id;
-    const saved = await blog.save();
-
-    user.blogs = user.blogs.concat(blog)
-    await user.save();
-
-    res.status(201).json(saved);
+        if(!title || !url) return res.status(400).json({error: "missing title or url"});
+        
+        const blog = new Blog(req.body);
+        blog.user = user._id;
+        const saved = await blog.save();
+    
+        user.blogs = user.blogs.concat(blog)
+        await user.save();
+    
+        res.status(201).json(saved); 
+    }
+    catch(e) {
+        next(e);
+    }
 })
 
 router.delete("/:id", async(req, res) => {
--- blog-list/utils/middleware.js
+++ blog-list/utils/middleware.js
@@ -1,11 +1,15 @@
 const errorHandler = (error, req, res, next) => {
     if (error.name === "CastError") return res.status(400).json({error: "malformed id"});
 
-    if(error.name === "ValidationError") return res.status(400).json({error: error.mesaage})
+    if(error.name === "ValidationError") return res.status(400).json({error: error.mesaage});
 
-    if(error.name === "JsonWebTokenError") return res.status(401).json({error: "invalid token"})
+    if(error.name === "JsonWebTokenError") {
+        return res.status(401).json({error: "invalid token"});
+    }
 
-    if(error.name === "TokenExpiredError") return res.status(401).json({error: "token expired"})
+    if(error.name === "TokenExpiredError") return res.status(401).json({error: "token expired"});
+
+    next();
 }
 
 const unknownEndpoint = (req, res) => res.status(400).json({error: "not found"})
