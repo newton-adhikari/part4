3c8ac50f460d9c08afaa037ab5d58368bbd0c75b
NewtonAdhikari <newtonadk@gmail.com>
2022-07-07 08:49:03 +0545

    bloglist expansion, step6 completed

--- blog-list/app.js
+++ blog-list/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const blogRouter = require("./controllers/blogRouter");
 const userRouter = require("./controllers/userRouter");
+const loginRouter = require("./controllers/login");
 const MONGODB_URI = require("./utils/config").MONGODB_URI;
 const {info, error} = require("./utils/logger");
 
@@ -16,5 +17,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/blogs", blogRouter);
 app.use("/api/users", userRouter);
+app.use("/api/login", loginRouter);
 
 module.exports = app;
